1042a7cea83d606b4bc48f2eaa5d36180696a922
Zyk <zyk6870@gmail.com>
2021-11-18 09:02:09 -0300

    Decrease Jetpack Upgrade cost
    Decreased Jetpack Upgrade cost when buying from the seller.
    
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -6478,7 +6478,7 @@ void Cmd_Stuff_f( gentity_t *ent ) {
 		}
 		else if (Q_stricmp(arg1, "upgrades" ) == 0)
 		{
-			trap->SendServerCommand( ent-g_entities, "print \"\n^38 - Gunner Radar: ^7Buy: 4000\n\n^315 - Impact Reducer: ^7Buy: 4000\n^316 - Flame Thrower: ^7Buy: 3000\n^325 - Power Cell Weapons Upgrade: ^7Buy: 2000\n^326 - Blaster Pack Weapons Upgrade: ^7Buy: 1800\n^327 - Metal Bolts Weapons Upgrade: ^7Buy: 2200\n^328 - Rocket Upgrade: ^7Buy: 2500\n^329 - Swimming Upgrade: ^7Buy: 2000\n^333 - Stun Baton Upgrade: ^7Buy: 1500\n^339 - Thermal Vision: ^7Buy: 2500\n^340 - Holdable Items Upgrade: ^7Buy: 3000\n^345 - Gunner Items Upgrade: ^7Buy: 3000\n^346 - Jetpack Upgrade: ^7Buy: 10000\n\n\"");
+			trap->SendServerCommand( ent-g_entities, "print \"\n^38 - Gunner Radar: ^7Buy: 4000\n\n^315 - Impact Reducer: ^7Buy: 4000\n^316 - Flame Thrower: ^7Buy: 3000\n^325 - Power Cell Weapons Upgrade: ^7Buy: 2000\n^326 - Blaster Pack Weapons Upgrade: ^7Buy: 1800\n^327 - Metal Bolts Weapons Upgrade: ^7Buy: 2200\n^328 - Rocket Upgrade: ^7Buy: 2500\n^329 - Swimming Upgrade: ^7Buy: 2000\n^333 - Stun Baton Upgrade: ^7Buy: 1500\n^339 - Thermal Vision: ^7Buy: 2500\n^340 - Holdable Items Upgrade: ^7Buy: 3000\n^345 - Gunner Items Upgrade: ^7Buy: 3000\n^346 - Jetpack Upgrade: ^7Buy: 8000\n\n\"");
 		}
 		else if (i == 1)
 		{
@@ -6715,7 +6715,7 @@ void Cmd_Buy_f( gentity_t *ent ) {
 	char arg1[MAX_STRING_CHARS];
 	int value = 0;
 	int found = 0;
-	int item_costs[NUMBER_OF_SELLER_ITEMS] = {15,20,25,40,80,120,150,4000,150,170,180,200,300,200,4000,3000,100,120,150,200,110,90,170,300,2000,1800,2200,2500,2000,200,200,20,1500,100,150,150,90,10,2500,3000,50,50,200,50,3000,10000,5000,700,2000,2000,2000,2000};
+	int item_costs[NUMBER_OF_SELLER_ITEMS] = {15,20,25,40,80,120,150,4000,150,170,180,200,300,200,4000,3000,100,120,150,200,110,90,170,300,2000,1800,2200,2500,2000,200,200,20,1500,100,150,150,90,10,2500,3000,50,50,200,50,3000,8000,5000,700,2000,2000,2000,2000};
 
 	if (trap->Argc() == 1)
 	{
