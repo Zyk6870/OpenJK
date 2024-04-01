49feaa908be8a92386bb8d90230d1a28a32fcea5
Zyk <zyk6870@gmail.com>
2024-04-01 19:41:46 -0300

    Bug fix
    Removed a line in w_force.c, the RPG level does not exist anymore.

--- codemp/game/w_force.c
+++ codemp/game/w_force.c
@@ -5382,7 +5382,7 @@ void sense_health_info(gentity_t *self, gentity_t *target)
 			max_magic_power = zyk_max_magic_power(target);
 		}
 
-		trap->SendServerCommand(self->s.number, va("cp \"%s\n^1%d^3/^1%d  ^2%d^3/^2%d\n^5%d^3/^5%d  ^7%d^3/^7%d\n^7Lvl %d\n\"", client_name, client_health, target->client->ps.stats[STAT_MAX_HEALTH], client_armor, client_max_armor, target->client->ps.fd.forcePower, target->client->ps.fd.forcePowerMax, magic_power, max_magic_power, target->client->pers.level));
+		trap->SendServerCommand(self->s.number, va("cp \"%s\n^1%d^3/^1%d  ^2%d^3/^2%d\n^5%d^3/^5%d  ^7%d^3/^7%d\n\"", client_name, client_health, target->client->ps.stats[STAT_MAX_HEALTH], client_armor, client_max_armor, target->client->ps.fd.forcePower, target->client->ps.fd.forcePowerMax, magic_power, max_magic_power));
 	}
 }
 
