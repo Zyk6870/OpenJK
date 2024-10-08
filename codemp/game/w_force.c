02bd88b56b2318fd7d048a27265bf17ecd96aeb5
Zyk <zyk6870@gmail.com>
2024-10-08 20:37:36 -0300

    Sense Health
    Added Stamina info to Sense Health skill.

--- codemp/game/w_force.c
+++ codemp/game/w_force.c
@@ -5340,6 +5340,8 @@ void sense_health_info(gentity_t *self, gentity_t *target)
 	char client_name[64];
 	int magic_power = 0;
 	int max_magic_power = 0;
+	int stamina = 0;
+	int max_stamina = 0;
 
 	if (!target || !target->client)
 	{ // zyk: for some reason, this guy has no client structure. Happens to quest_ragnos npc
@@ -5360,6 +5362,8 @@ void sense_health_info(gentity_t *self, gentity_t *target)
 		if (target->client->sess.amrpgmode == 2)
 		{
 			client_max_health = target->client->pers.max_rpg_health;
+			stamina = target->client->pers.current_stamina;
+			max_stamina = target->client->pers.max_stamina;
 		}
 
 		strcpy(client_name, target->client->pers.netname);
@@ -5389,7 +5393,9 @@ void sense_health_info(gentity_t *self, gentity_t *target)
 			max_magic_power = zyk_max_magic_power(target);
 		}
 
-		trap->SendServerCommand(self->s.number, va("cp \"%s\n^1%d^3/^1%d  ^2%d^3/^2%d\n^5%d^3/^5%d  ^7%d^3/^7%d\n\"", client_name, client_health, client_max_health, client_armor, client_max_armor, target->client->ps.fd.forcePower, target->client->ps.fd.forcePowerMax, magic_power, max_magic_power));
+		trap->SendServerCommand(self->s.number, va("cp \"%s\n^1%d^3/^1%d  ^2%d^3/^2%d\n^5%d^3/^5%d  ^7%d^3/^7%d\n^3%d/%d\n\"", 
+			client_name, client_health, client_max_health, client_armor, client_max_armor, target->client->ps.fd.forcePower, target->client->ps.fd.forcePowerMax, 
+			magic_power, max_magic_power, stamina, max_stamina));
 	}
 }
 
