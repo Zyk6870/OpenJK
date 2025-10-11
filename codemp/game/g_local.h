04a9b2f71001529e26f26fb1788af02f0342e205
Zyk <zyk6870@gmail.com>
2025-10-11 14:55:27 -0300

    Run Speed skill
    Decreased speed bonus a little.

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -44,7 +44,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"New Zyk Mod v1.5.23"
+#define	GAMEVERSION	"New Zyk Mod v1.5.24"
 
 #define SECURITY_LOG "security.log"
 
@@ -855,7 +855,7 @@ typedef enum {
 // zyk: npc cloak detection distance
 #define NPC_CLOAK_DETECTION_DISTANCE 1000
 
-#define RPG_RUN_SPEED_SKILL_INCREASE 10.0f
+#define RPG_RUN_SPEED_SKILL_INCREASE 5.0f
 
 // zyk: main characters names
 #define QUESTCHAR_MAIN "^6Spirit Tree"
